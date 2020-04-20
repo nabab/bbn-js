844c4e2a34469312602fc2d5b8810c5361415af7
nabab <thomas.nabet@gmail.com>
2020-04-20 07:19:59 +0200

    Added documentation

--- src/fn/object.js
+++ src/fn/object.js
@@ -1980,7 +1980,6 @@
      *   title: "Once upon a time in the west",
      *   synopsis: "There's a single piece of land around Flagstone with water on it, and rail baron Morton (Gabriele Ferzetti) aims to have it, knowing the new railroad will have to stop there. He sends his henchman Frank (Henry Fonda) to scare the land's owner, McBain (Frank Wolff), but Frank kills him instead and pins it on a known bandit, Cheyenne (Jason Robards). Meanwhile, a mysterious gunslinger with a score to settle (Charles Bronson) and McBain's new wife, Jill (Claudia Cardinale), arrive in town."
      * }, 50)
-     * })
      * // {
      * //   "title": "Once upon a time in the west",
      * //   "synopsis": "There's a single piece of land around Flagstone wi..."
