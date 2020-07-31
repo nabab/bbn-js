a409b2bbe3fc1097a838e6d0d11c4e9f1f71a52a
Thomas Nabet <thomas.nabet@gmail.com>
2020-07-31 17:17:46 +0200

    Added selectElementText function

--- src/fn/misc.js
+++ src/fn/misc.js
@@ -733,7 +733,50 @@
      */
     isFocused(ele, contain) {
       return (ele === document.activeElement) || (contain && ele.contains && (ele.contains(document.activeElement)));
-    }
+    },
+
+    /**
+     * Selects the content of an element.
+     * 
+     * @method   selectElementText
+     * @global   
+     * @example
+     * ``` javascript
+     * bbn.fn.selectElementText(document.getElementById('my_input_id'));
+     * // false
+     * bbn.fn.selectElementText(bbn.$('#my_span_id'));
+     * // true
+     * ```
+     * @memberof bbn.fn
+     * 
+     * @param {Element} ele The element in which the text should be selected
+     * @param {Boolean} win The window object
+     * 
+     * @returns  {Boolean} True if focused
+     */
+    selectElementText(ele, win) {
+      win = win || window;
+      if (ele instanceof HTMLInputElement) {
+        ele.select();
+        return;
+      }
+      let doc = win.document;
+      let sel;
+      let range;
+
+      if (win.getSelection && doc.createRange) {
+        sel = win.getSelection();
+        range = doc.createRange();
+        range.selectNodeContents(ele);
+        sel.removeAllRanges();
+        sel.addRange(range);
+      }
+      else if (doc.body.createTextRange) {
+        range = doc.body.createTextRange();
+        range.moveToElementText(ele);
+        range.select();
+      }
+  }
 
   });
 })(bbn);
