b3a370929c2be621a9decafbb45f381d9fa35d28
Thomas Nabet <thomas.nabet@gmail.com>
2020-08-15 19:44:05 +0200

    Changed webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const glob = require('glob');
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
 
 module.exports = [{
   mode: 'development',
@@ -10,40 +8,4 @@ module.exports = [{
     libraryTarget: 'commonjs',
     filename: 'bbn.js',
   }
-}, {
-  mode: 'development',
-  entry: createLessEntryPoints(),
-  plugins: [new FixStyleOnlyEntriesPlugin(), new MiniCssExtractPlugin({filename: '[name].css'})],
-  module: {
-    rules: [
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/'
-            }
-          }
-        ]
-      },
-      {
-        test: /\.less$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader']
-      }
-    ],
-  }
 }];
-
-function createLessEntryPoints() {
-  const lessFiles = glob.sync('./nodejs/css/**/*.less');
-
-  const lessEntryPoints = {};
-  lessFiles.forEach(file => {
-    const basename = path.basename(file);
-    const filenameWithoutExtension = path.parse(basename).name;
-    lessEntryPoints[filenameWithoutExtension] = file;
-  });
-  return lessEntryPoints;
-}
