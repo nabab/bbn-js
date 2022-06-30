3e6761c751c4eaf84cd63ce8509def2fadc5ce20
nabab <thomas.nabet@gmail.com>
2022-06-30 17:50:50 +0200

    Added shuffle function

--- src/fn/object.js
+++ src/fn/object.js
@@ -1569,6 +1569,29 @@
     },
 
     /**
+     * 
+     * @param {Array} array 
+     * @returns Array
+     */
+    shuffle(array) {
+      let currentIndex = array.length,  randomIndex;
+    
+      // While there remain elements to shuffle.
+      while (currentIndex != 0) {
+    
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+    
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex], array[currentIndex]];
+      }
+    
+      return array;
+    },
+
+    /**
      * Returns an object describing the differences for transforming the first given object into the second.
      * 
      * The returned data will use the objects properties as keys. If unchanged is set to true, all the 
