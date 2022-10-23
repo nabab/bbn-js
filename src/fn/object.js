266614f7117457b5f479f075b4070d6c1ac36682
nabab <thomas.nabet@gmail.com>
2022-10-23 14:42:34 +0200

    Added getFieldValues

--- src/fn/object.js
+++ src/fn/object.js
@@ -587,6 +587,45 @@
     },
 
     /**
+     * Returns all the unique values of the given field (property) from the first object matching the given filter in an array.
+     *
+     * The filtering arguments follow the same scheme as bbn.fn.search.
+     *
+     * @method   getFieldValues
+     * @global
+     * @example
+     * ```javascript
+     * let ar = [
+     *   {name: "Raiders of the lost ark", director: "Steven Spielberg", year: 1981, id: 589},
+     *   {name: "Star wars", director: "George Lucas", year: 1977, id: 256},
+     *   {name: "Jaws", director: "Steven Spielberg", year: 1975, id: 423}
+     *   {name: "Barry Lindon", director: "Stanley Kubrick", year: 1975, id: 802}
+     * ];
+     * bbn.fn.getFieldValues(ar, "director");
+     * // ["Steven Spielberg", "George Lucas", "Stanley Kubrick"]
+     * bbn.fn.getFieldValues(ar, "name", {year: 1975});
+     * // ["Jaws", "Barry Lindon"]
+     * ```
+     * @memberof bbn.fn
+     * @param    {Array}                    arr       The subject array
+     * @param    {String}                   field     The property from which the values are returned
+     * @param    {(String|Object|Function)} prop      A property's name or a filter object or function
+     * @param    {*}                        val       The value with which comparing the given property
+     * @param    {String}                   operator  The operator to use for comparison with the value as used in bbn.fn.compare
+     * @returns  {*}
+     */
+    getFieldValues(arr, field, prop, val, operator) {
+      bbn.fn.checkType(field, 'string');
+      if (prop) {
+        arr = bbn.fn.filter(arr, prop, val, operator, false);
+      }
+
+      let res = [];
+      bbn.fn.each(arr, a => res.indexOf(a[field]) === -1 ? res.push(a[field]) : null);
+      return res;
+    },
+
+    /**
      * Retrieves all elements of a hierarchical array corresponding to the filter.
      * 
      * The arguments follow the same scheme as bbn.fn.search.
