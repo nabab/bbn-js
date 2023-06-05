ca8898c26eecf66ae5502df6fb115a2cd5946ed9
Mirko Argentino <mirko@bbn.solutions>
2023-06-05 15:27:29 +0200

    Added getBrowserName and getBrowserVersion functions

--- src/fn/misc.js
+++ src/fn/misc.js
@@ -887,6 +887,76 @@
     },
 
     /**
+     * Gets the browser's name
+     * @method getBrowserName
+     * @global
+     * @memberof bbn.fn
+     * @returns {String}
+     */
+    getBrowserName(){
+      const userAgent = navigator.userAgent.toLowerCase();
+      switch (true) {
+        case userAgent.includes('edge'):
+        case userAgent.includes('edg/'):
+          return "Edge";
+
+        case userAgent.includes('opr') && !!window.opr:
+          return "Opera";
+
+        case userAgent.includes('chrome') && !!window.chrome:
+          return "Chrome";
+
+        case userAgent.includes('trident'):
+          return "Internet Explorer";
+
+        case userAgent.includes('firefox'):
+          return "Firefox";
+
+        case userAgent.includes('safari'):
+          return "Safari";
+
+        default:
+          return "Other";
+      }
+    },
+
+    /**
+     * Gets the browser's version
+     * @method getBrowserVersion
+     * @global
+     * @memberof bbn.fn
+     * @returns {String}
+     */
+    getBrowserVersion(){
+      const userAgent = navigator.userAgent.toLowerCase();
+      switch (true) {
+        case userAgent.includes('edge/'):
+          return userAgent.split('edge/')[1].split(' ')[0];
+
+        case userAgent.includes('edg/'):
+          return userAgent.split('edg/')[1].split(' ')[0];
+
+        case userAgent.includes('opr/') && !!window.opr:
+          return userAgent.split('opr/')[1].split(' ')[0];
+
+        case userAgent.includes('chrome/') && !!window.chrome:
+          return userAgent.split('chrome/')[1].split(' ')[0];
+
+        case userAgent.includes('trident/'):
+          return userAgent.split('trident/')[1].split(' ')[0];
+
+        case userAgent.includes('firefox/'):
+          return userAgent.split('firefox/')[1].split(' ')[0];
+
+        case userAgent.includes('safari/'):
+          return userAgent.split('version/')[1].split(' ')[0];
+
+        default:
+          return '';
+      }
+    },
+
+    /**
      * Returns the current device type.
      * @method   getDeviceType
      * @global
