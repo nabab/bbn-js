141b2ed5d92e1470149c67d3cb57e569163b681e
nabab <thomas.nabet@gmail.com>
2023-10-08 01:17:05 +0200

    Latest changes

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import { _ } from './_.ts';
-import { $ } from './$.ts';
-import { lng } from './lng.ts';
-import { vars } from './vars.ts';
-import { env } from './env.ts';
-import { db } from './db.ts';
-import { fn } from './fn.ts';
+import { _ } from './_.js';
+import { $ } from './$.js';
+import { lng } from './lng.js';
+import { vars } from './vars.js';
+import { env } from './env.js';
+import { db } from './db.js';
+import { fn } from './fn.js';
 
-const bbn = {
+const bbn: Bbn = {
   version: "1.0.1",
   opt: {
     _cat: {}
