2a39c0136addad8ab2209f9b9e926210416bfb2a
nabab <thomas.nabet@gmail.com>
2023-10-07 18:27:19 +0200

    Latest changes

--- dist/fn/datetime/calendar.js
+++ dist/fn/datetime/calendar.js
@@ -1,9 +1,9 @@
 import dayjs from '../../../node_modules/dayjs/dayjs.min.js';
+import dayjs_plugin_calendar from '../../../node_modules/dayjs/plugin/calendar.js';
 import { fdate } from "./fdate.js";
 import { date } from "./date.js";
 import { isDate } from "../type/isDate.js";
 import { isString } from "../type/isString.js";
-import dayjs_plugin_calendar from '../../../node_modules/dayjs/plugin/calendar.js';
 const bbn = {
     _: st => st
 };
