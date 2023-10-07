b8b77ab29d26f15c5effaa1fe1c2b7a0644774b6
nabab <thomas.nabet@gmail.com>
2023-10-07 03:58:46 +0200

    Latest changes

--- dist/bundle.js
+++ dist/bundle.js
@@ -1371,14 +1371,14 @@
             if (!args.length) {
                 return false;
             }
-            if ('cp' in bbn && 'isComponent' in bbn['cp'] && (typeof bbn['cp'].isComponent === 'function')) {
-                for (let a of args) {
-                    let res = bbn.cp.isComponent(a);
-                    if (!res || (0, isDom_1.isDom)(a) || !((_a = a.$el) === null || _a === void 0 ? void 0 : _a.bbnCid)) {
-                        return false;
-                    }
-                }
+            if (!('cp' in bbn) || !('isComponent' in bbn['cp']) || !(typeof bbn['cp'].isComponent === 'function')) {
                 return false;
+            }
+            for (let a of args) {
+                let res = bbn.cp.isComponent(a);
+                if (!res || (0, isDom_1.isDom)(a) || !((_a = a.$el) === null || _a === void 0 ? void 0 : _a.bbnCid)) {
+                    return false;
+                }
             }
             return true;
         };
