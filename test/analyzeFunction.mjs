b3ccc4e087f59a6a021e7a635a43567ae687b83f
nabab <thomas.nabet@gmail.com>
2023-10-07 14:11:53 +0200

    Latest changes

---
+++ test/analyzeFunction.mjs
@@ -0,0 +1,24 @@
+import {expect} from 'chai';
+import {} from 'jsdom-global';
+import {} from '../dist/fn/misc/analyzeFunction.js';
+
+describe('analyzeFunction', function() {
+
+    it('Should analyze a nested function correctly', function() {
+        const testFunction = `
+            function outer() {
+                return function inner() {};
+            }
+        `;
+
+        const result = analyzeFunction(testFunction);
+
+        // Making a basic assertion, you can make more assertions based on your requirements
+        expect(result.name).to.equal('outer');
+        // ... more assertions based on other properties of the expected result
+    });
+
+    // Add more tests as needed
+
+});
+
--- test/type.spec.mjs
+++ test/type.spec.mjs
@@ -1,59 +1,59 @@
 import {expect} from 'chai';
-import {} from 'jsdom-global';
-import {} from '../dist/fn/type/isEmail.js';
-import {} from '../dist/fn/type/isColor.js';
-import {} from '../dist/fn/type/isDom.js';
+import {} from 'jsdom';
+import {isEmail} from '../dist/fn/type/isEmail.js';
+import {isColor} from '../dist/fn/type/isColor.js';
+import {isDom} from '../dist/fn/type/isDom.js';
 
 describe(`Type Functions`, () => {
   describe(`isEmail`, () => {
     it('should determine if text is an email', () => {
-      expect(bbn.fn.isEmail('test@test.org')).to.be.true;
-      expect(bbn.fn.isEmail('something+else@example.com')).to.be.true;
+      expect(isEmail('test@test.org')).to.be.true;
+      expect(isEmail('something+else@example.com')).to.be.true;
 
-      expect(bbn.fn.isEmail('hello@a')).to.be.false;
-      expect(bbn.fn.isEmail('hello@a.b')).to.be.false;
-      expect(bbn.fn.isEmail('example.com')).to.be.false;
-      expect(bbn.fn.isEmail('test@testorg')).to.be.false;
-      expect(bbn.fn.isEmail('@example.com')).to.be.false;
-      expect(bbn.fn.isEmail('something@example')).to.be.false;
+      expect(isEmail('hello@a')).to.be.false;
+      expect(isEmail('hello@a.b')).to.be.false;
+      expect(isEmail('example.com')).to.be.false;
+      expect(isEmail('test@testorg')).to.be.false;
+      expect(isEmail('@example.com')).to.be.false;
+      expect(isEmail('something@example')).to.be.false;
     });
 
     it('should check for types other than string', () => {
-      expect(bbn.fn.isEmail({})).to.be.false;
-      expect(bbn.fn.isEmail([])).to.be.false;
-      expect(bbn.fn.isEmail([{}])).to.be.false;
-      expect(bbn.fn.isEmail(() => {})).to.be.false;
+      expect(isEmail({})).to.be.false;
+      expect(isEmail([])).to.be.false;
+      expect(isEmail([{}])).to.be.false;
+      expect(isEmail(() => {})).to.be.false;
     });
   });
 
   describe(`isColor`, () => {
     it('should determine if text is a color', () => {
-      expect(bbn.fn.isColor("red")).to.be.true;
-      expect(bbn.fn.isColor("#FF0000")).to.be.true;
-      expect(bbn.fn.isColor("rgb 255, 0, 0")).to.be.true;
+      expect(isColor("red")).to.be.true;
+      expect(isColor("#FF0000")).to.be.true;
+      expect(isColor("rgb 255, 0, 0")).to.be.true;
 
-      expect(bbn.fn.isColor("#FF00")).to.be.false;
-      expect(bbn.fn.isColor("#FF00FF00")).to.be.false;
+      expect(isColor("#FF00")).to.be.false;
+      expect(isColor("#FF00FF00")).to.be.false;
     });
 
     it('should check for types other than string', () => {
-      expect(bbn.fn.isColor({})).to.be.false;
-      expect(bbn.fn.isColor([])).to.be.false;
-      expect(bbn.fn.isColor([{}])).to.be.false;
-      expect(bbn.fn.isColor(() => {})).to.be.false;
+      expect(isColor({})).to.be.false;
+      expect(isColor([])).to.be.false;
+      expect(isColor([{}])).to.be.false;
+      expect(isColor(() => {})).to.be.false;
     });
   });
 
   describe(`isDom`, () => {
     it('should determine if element is a DOM element', () => {
-      expect(bbn.fn.isDom(document.createElement("div"))).to.be.true;
-      expect(bbn.fn.isDom(document.createElement("p"))).to.be.true;
-      expect(bbn.fn.isDom(window)).to.be.false;
+      expect(isDom(document.createElement("div"))).to.be.true;
+      expect(isDom(document.createElement("p"))).to.be.true;
+      expect(isDom(window)).to.be.false;
 
-      expect(bbn.fn.isDom({})).to.be.false;
-      expect(bbn.fn.isDom([])).to.be.false;
-      expect(bbn.fn.isDom([{}])).to.be.false;
-      expect(bbn.fn.isDom(() => {})).to.be.false;
+      expect(isDom({})).to.be.false;
+      expect(isDom([])).to.be.false;
+      expect(isDom([{}])).to.be.false;
+      expect(isDom(() => {})).to.be.false;
     });
   });
 });
