259692ef400dc1c99d2548538094ad74e68a54e2
nabab <thomas.nabet@gmail.com>
2023-10-23 10:41:25 +0200

    Latest changes

--- dist/bbn.js
+++ dist/bbn.js
@@ -11257,7 +11257,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var simpleHash = function (str) {
     var part1 = (0,_simpleHash1_js__WEBPACK_IMPORTED_MODULE_0__.simpleHash1)(str).toString(16).padStart(8, '0');
-    var part2 = (0,_simpleHash2_js__WEBPACK_IMPORTED_MODULE_1__.simpleHash2)(str).toString(16).padStart(8, '0');
+    var part2 = (0,_simpleHash2_js__WEBPACK_IMPORTED_MODULE_1__.simpleHash2)(str).padStart(8, '0');
     return part1 + part2;
 };
 
@@ -11308,7 +11308,7 @@ var simpleHash2 = function (str) {
         hash = char + (hash << 6) + (hash << 16) - hash;
         hash |= 0; // Convert to 32-bit integer
     }
-    return Math.abs(hash);
+    return Math.abs(hash).toString(16);
 };
 
 
