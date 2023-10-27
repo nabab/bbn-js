d6a90915715a17907312c27b4d9906f2faa27d37
nabab <thomas.nabet@gmail.com>
2023-10-27 17:05:02 +0200

    Latest changes

---
+++ user-nabab-light.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -rf dist
+npm run type
+npm run build
+git stage ./*
+git stage -f ./dist
+npm version patch
+git stage ./package.json
+git commit -m "Latest changes"
+git push
+npm publish
+rm -rf ~/web/bbn-js/v2/dist
+cp -rf dist ~/web/bbn-js/v2/
+
+
+
+
