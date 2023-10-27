20a8dabf825de1a368d4df49d5a7d312471a2adf
nabab <thomas.nabet@gmail.com>
2023-10-27 17:50:42 +0200

    Latest changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+import path from 'path';
+import { URL } from 'url'; // in Browser, the URL in native accessible on window
+
 var config = {};
 
 function generateConfig(name) {
@@ -25,3 +28,68 @@ function generateConfig(name) {
 
 export default config;
 
+
+
+
+/*
+import path from 'path';
+import { URL } from 'url'; // in Browser, the URL in native accessible on window
+
+const __filename = new URL('', import.meta.url).pathname;
+// Will contain trailing slash
+const __dirname = new URL('.', import.meta.url).pathname;
+import webpack from 'webpack';
+
+export default {
+    mode: "development",
+    // Entry file(s)
+    entry: './dist/index.js',
+
+    // Output configuration
+    output: {
+        path: __dirname  + 'dist',
+        filename: 'bbn.js',
+        library: {
+            name: 'bbn',
+            type: 'global'
+        },
+    },
+
+    // Set up loaders
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+
+    // Resolve extensions and modules
+    resolve: {
+        extensions: ['.js'],
+        modules: [
+            __dirname + 'dist',
+            'node_modules'
+        ]
+    },
+
+    // Plugins (if you have any)
+    plugins: [
+      new webpack.DefinePlugin({
+        IS_TESTING: JSON.stringify(process.env.IS_TESTING)
+      })
+    ],
+    // Optional: Development server configuration
+    // devServer: {
+    //     contentBase: path.join(__dirname, 'public'),
+    //     compress: true,
+    //     port: 9000
+    // },
+
+    // Optional: Source maps for development
+    devtool: 'source-map',
+};
+
+*/
