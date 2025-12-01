ab47968754dc7295b5bd8146e7cdf9fd40503cef
nabab <thomas.nabet@gmail.com>
2025-12-01 11:55:57 +0100

    Latest changes

--- dist/dt/classes/dt.js
+++ dist/dt/classes/dt.js
@@ -1067,18 +1067,18 @@ export class bbnDt {
     daysInMonth() {
         if (this.isValid) {
             switch (this.month()) {
-                case 1:
+                case 2:
                     if (this.year() % 4 === 0) {
                         return 29;
                     }
                     else {
                         return 28;
                     }
-                case 0:
-                case 3:
-                case 5:
-                case 8:
-                case 10:
+                case 1:
+                case 4:
+                case 6:
+                case 9:
+                case 11:
                     return 30;
                 default:
                     return 31;
