0809ac54a48c79fd831e6797de3da7c768c2da9c
nabab <thomas.nabet@gmail.com>
2025-12-10 18:10:33 +0100

    Latest changes

---
+++ dist/dt/data/weekinfo-overrides.d.ts
@@ -0,0 +1,573 @@
+declare const _default: {
+    AE: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    AF: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    AG: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    AI: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    AL: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    AM: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    AR: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    AS: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    AU: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    AZ: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    BA: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    BD: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    BH: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    BM: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    BN: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    BR: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    BS: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    BT: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    BW: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    BY: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    BZ: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    CA: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    CL: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    CM: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    CN: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    CO: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    CR: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    CY: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    DJ: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    DM: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    DO: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    DZ: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    EC: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    EG: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    ET: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    "GB-alt-variant": {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    GE: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    GT: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    GU: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    HK: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    HN: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    HR: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    ID: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    IL: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    IN: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    IQ: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    IR: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    IS: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    JM: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    JO: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    JP: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    KE: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    KG: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    KH: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    KR: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    KW: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    KZ: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    LA: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    LB: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    LK: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    LV: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    LY: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    MD: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    ME: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    MH: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    MK: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    MM: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    MN: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    MO: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    MT: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    MV: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    MX: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    MY: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    MZ: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    NI: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    NP: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    NZ: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    OM: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    PA: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    PE: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    PH: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    PK: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    PR: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    PT: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    PY: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    QA: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    RO: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    RS: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    SA: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    SD: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    SG: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    SI: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    SV: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    SY: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    TH: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    TJ: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    TM: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    TR: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    TT: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    TW: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    UA: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    UG: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    UM: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    US: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    UY: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    UZ: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    VE: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    VI: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    VN: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    WS: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    XK: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    YE: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    ZA: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+    ZW: {
+        firstDay: number;
+        weekend: number[];
+        minimalDays: number;
+    };
+};
+export default _default;
