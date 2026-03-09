887627e2313fb3473f8b4795fa6e7352eeef435d
nabab <thomas.nabet@gmail.com>
2026-03-09 01:01:01 +0100

    Latest changes

--- dist/fn/ajax/link.js
+++ dist/fn/ajax/link.js
@@ -45,6 +45,7 @@ export default function link(...args) {
         return true;
     }
     if (cfg.url.indexOf('#') === 0) {
+        debugger;
         if (cfg.e) {
             cfg.e.preventDefault();
         }
